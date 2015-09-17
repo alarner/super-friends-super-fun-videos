6a725abf6c8465420f76b7a43fbfba60e16866f6
Leslie Sutton <l4sutton@gmail.com>
2015-09-17 10:33:40 -0500

    added submit event listener

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -3,15 +3,34 @@
 
 $(document).ready(function () {
 
-	var $viewVideos = $('#view');
-	var $superVideos = $('#superVideos');
-	var $form = $('#form');
+    var $viewVideos = $('#view');
+    var $superVideos = $('#superVideos');
+    var $form = $('#form');
+    var $description = $('#description');
+    var $title = $('#title');
+    var $videoEmbed = $('#videoEmbed');
+    var $add = $('#add');
 
-	$viewVideos.click(function () {
-		$form.hide();
-		$superVideos.css({ display: 'block' });
-		console.log('It works!');
-	});
+    $viewVideos.click(function () {
+        $form.hide();
+        $superVideos.show();
+    });
+
+    $add.click(function () {
+        $form.show();
+        $superVideos.hide();
+    });
+
+    $form.submit(function (e) {
+        e.preventDefault();
+        var newTitle = $title.val();
+        var newVideo = $videoEmbed.val();
+        var newDesc = $description.val();
+
+        console.log(newTitle);
+        console.log(newVideo);
+        console.log(newDesc);
+    });
 });
 
 },{}]},{},[1])
--- scripts/main.js
+++ scripts/main.js
@@ -6,16 +6,32 @@ $(document).ready(function() {
     var $superVideos = $('#superVideos');
     var $form = $('#form');
     var $description = $('#description');
+    var $title = $('#title');
+    var $videoEmbed = $('#videoEmbed')
     var $add = $('#add');
 
     $viewVideos.click(function() {
         $form.hide();
         $superVideos.show();
+
     })
 
     $add.click(function() {
     	$form.show();
     	$superVideos.hide();
+    })
+
+    $form.submit(function(e){
+        e.preventDefault();
+        var newTitle = $title.val();
+        var newVideo = $videoEmbed.val();
+        var newDesc = $description.val();
+
+        console.log(newTitle);
+        console.log(newVideo);
+        console.log(newDesc);
+
+
     })
 
 
