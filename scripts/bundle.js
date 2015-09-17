297a1e71ea047e8694b49bcf8efba533a24c8593
Aaron Larner <anlarner@gmail.com>
2015-09-17 10:11:26 -0500

    updated link to hide-add-video-form

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -5,8 +5,10 @@ $(document).ready(function () {
 
 	var $viewVideos = $('#view');
 	var $superVideos = $('#superVideos');
+	var $form = $('#form');
 
 	$viewVideos.click(function () {
+		$form.hide();
 		$superVideos.css({ display: 'block' });
 		console.log('It works!');
 	});
--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,11 @@ $(document).ready(function() {
 
 var $viewVideos = $('#view');
 var $superVideos = $('#superVideos');
+var $form = $('#form');
+
 
 $viewVideos.click(function() {
+	$form.hide();
 	$superVideos.css({display: 'block'});
 	console.log('It works!');
 })
