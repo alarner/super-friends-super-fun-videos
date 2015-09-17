052e496da8181924b63cee8fb8378a7bd3329640
Aaron Micko <gmicko@hotmail.com>
2015-09-17 09:58:55 -0500

    unhid section with link click

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+$(document).ready(function() {
+
+var $viewVideos = $('#view');
+var $superVideos = $('#superVideos');
+
+$viewVideos.click(function() {
+	$superVideos.css({display: 'block'});
+	console.log('It works!');
+})
+
+
+}) 
